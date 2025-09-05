1daeb256d4bc9fb6ef2d0f450cd6d4700da49891
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-09-04 22:02:12 -0300

    feat: create tsconfig.build

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
-import { resolve } from "path";
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    dts()
+    dts({
+      tsconfigPath: 'tsconfig.build.json',
+      insertTypesEntry: true,
+      copyDtsFiles: false,
+      rollupTypes: true,
+      outDir: 'dist'
+    }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "kickoff-ui",
-      fileName: "kickoff-ui",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'kickoff-ui',
+      fileName: 'kickoff-ui',
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react/jsx-runtime": "react/jsx-runtime",
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
     },
