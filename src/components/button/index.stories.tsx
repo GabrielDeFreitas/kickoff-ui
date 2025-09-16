3ee1dbbaef804f92672835d842ab7d5477a3057c
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-09-15 23:57:09 -0300

    refactor: button component

--- src/components/button/index.stories.tsx
+++ src/components/button/index.stories.tsx
@@ -3,7 +3,6 @@ import { Button } from '.';
 
 const meta: Meta<typeof Button> = {
     component: Button,
-    title: 'Components/Button',
     tags: ['autodocs'],
     argTypes: {
         label: {
@@ -16,8 +15,45 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Default: Story = {
+const Icon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+    >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+    </svg>
+);
+
+
+export const Primary: Story = {
     args: {
+        icon: Icon,
+        variant: 'primary',
+        label: 'Click here',
+    },
+};
+
+export const Secondary: Story = {
+    args: {
+        variant: 'secondary',
+        label: 'Click here',
+    },
+};
+
+export const Danger: Story = {
+    args: {
+        variant: 'danger',
+        label: 'Click here',
+    },
+};
+
+export const Success: Story = {
+    args: {
+        variant: 'success',
         label: 'Click here',
     },
 };
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,22 @@
-interface ButtonProps {
+import { ButtonHTMLAttributes, ElementType } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     label: string;
+    icon?: ElementType;
+    variant?: "primary" | "secondary" | "danger" | "success";
 }
 
-export function Button({ label }: ButtonProps) {
+const buttonVariants = {
+    primary: "bg-stone-900 text-white hover:bg-stone-800 focus:ring-stone-950/10",
+    secondary: "bg-stone-200 text-stone-900 focus:ring-stone-950/10",
+    danger: "bg-red-600 text-white hover:bg-red-800 focus:ring-red-950/10",
+    success: "bg-emerald-500 text-white hover:bg-emerald-600 focus:ring-green-950/10",
+};
+
+export function Button({ icon: Icon, label, variant = 'primary', ...rest }: ButtonProps) {
     return (
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        <button  {...rest} className={`cursor-pointer inline-flex items-center justify-center rounded-md text-sm font-medium px-4 py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 ${buttonVariants[variant]} ${rest.className}`}>
+            {Icon && <Icon className="w-5 h-5 text-zinc-50 mr-1" />}
             {label}
         </button>
     );
